c35112f8f9393a2a2269277f9aafec9a15e250cb
Achintya-Tiwari <136591124+Achintya-Tiwari@users.noreply.github.com>
2023-06-21 15:42:13 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,9 @@
+function calculateArea(length, width) {
+    var area = length * width;
+    return area;
+  }
+  
+  var length = 5;
+  var width = 8;
+  var area = calculateArea(length, width);
+  console.log("The area of the rectangle is: " + area);
